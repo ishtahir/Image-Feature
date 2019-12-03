b369e2834fb30b640960b0a60928803029f3f3aa
Ish Tahir <tahirismaeel@gmail.com>
2019-12-03 12:56:35 -0600

    Added modal bg, contents, close btn

--- client/components/Modal.jsx
+++ client/components/Modal.jsx
@@ -4,7 +4,26 @@ const Modal = props => {
   if (!props.showModal) {
     return null;
   } else {
-    return <h1>Hello from modal</h1>;
+    return (
+      <div className="modal-bg">
+        <div className="modal-content">
+          <div className="close-btn">×</div>
+          <h1>Testing Modal</h1>
+          <button>
+            <i class="fas fa-chevron-left"></i>
+          </button>
+          <div className="text-container">
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Et magni beatae libero, quaerat praesentium nulla magnam quae consectetur?
+              Perferendis, voluptates.
+            </span>
+          </div>
+          <button>
+            <i className="fas fa-chevron-right"></i>
+          </button>
+        </div>
+      </div>
+    );
   }
 };
 

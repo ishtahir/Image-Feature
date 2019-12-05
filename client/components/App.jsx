7bae02b9c1ed431bfc33df9d9ff34faf6c4bd7ed
Ish Tahir <tahirismaeel@gmail.com>
2019-12-04 18:38:41 -0600

    Added changePic function

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,19 +8,46 @@ class App extends Component {
     super();
     this.state = {
       main: 'img1.jpg',
-      images: ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg']
+      images: ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg'],
+      showModal: false,
+      modalMain: ''
     };
   }
 
   mouseEnterHandler(evt) {
-    this.setState({ main: evt.target.dataset.src });
+    this.setState({ main: evt.target.dataset.src, modalMain: evt.target.dataset.src });
+  }
+
+  toggleModal(img) {
+    this.setState({ showModal: !this.state.showModal, modalMain: img });
+  }
+
+  changePic(src) {
+    this.setState({ modalMain: src });
   }
 
   render() {
     return (
       <div className="container">
-        <MainImage img={this.state.main} />
-        <ImageContainer images={this.state.images} mouseEnterHandler={this.mouseEnterHandler.bind(this)} main={this.state.main} />
+        <MainImage
+          img={this.state.main}
+          toggleModal={this.toggleModal.bind(this)}
+          changePic={this.changePic.bind(this)}
+          showModal={this.state.showModal}
+        />
+        <ImageContainer
+          images={this.state.images}
+          mouseEnterHandler={this.mouseEnterHandler.bind(this)}
+          main={this.state.main}
+          toggleModal={this.toggleModal.bind(this)}
+        />
+        <Modal
+          showModal={this.state.showModal}
+          toggleModal={this.toggleModal.bind(this)}
+          images={this.state.images}
+          changePic={this.changePic.bind(this)}
+          modalMain={this.state.modalMain}
+        />
       </div>
     );
   }
